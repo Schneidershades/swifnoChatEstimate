1d06ecccb259236ee3a6f976a6e465fa4ede482c
Schneider Shades <schneidershades@gmail.com>
2021-01-10 19:27:17 +0100

    push

--- app/Http/Controllers/EstimateController.php
+++ app/Http/Controllers/EstimateController.php
@@ -443,7 +443,7 @@ class EstimateController extends Controller
 
     public function sendWhatsAppMessage(string $message, string $recipient)
     {
-        $twilio_whatsapp_number = '+14155238886';
+        $twilio_whatsapp_number = '+1 415 523 8886';
         $account_sid = 'AC02dd6e16114e3fe1db4e2e5ce134fd8e';
         $auth_token = 'ee3930f1ae4d3a6737ca79c9d0b8feae';
 
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::resource('/estimate-chatbot', 'EstimateController');
+
